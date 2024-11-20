c81dcb54cf28d888daf6ce137eb510fd394cc553
wsz <1245306668@qq.com>
2024-11-20 11:32:41 +0800

    二级指针

--- cpp/algrithm/secondPointer.cpp
+++ cpp/algrithm/secondPointer.cpp
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[])
         b[i]=new int[8];
     }
     b[2][0]=2;
-    //由此可见，二级指针前进一位，相当于一级指针的数组前进一整行
+    //由此可见，二级指针前进一位，相当于一级指针的数组前进一整行  
     std::cout<<**(b+2)<<std::endl;
     return 0;
 }
