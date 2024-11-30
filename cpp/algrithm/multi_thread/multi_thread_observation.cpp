58c4e585a435608d2ae4a38af07c4eb5ff2db5c5
YOSHIKII666 <1245306668@qq.com>
2024-11-30 23:59:09 +0800

    多线程观察者

--- cpp/algrithm/multi_thread/multi_thread_observation.cpp
+++ cpp/algrithm/multi_thread/multi_thread_observation.cpp
@@ -1,105 +1,94 @@
-#include<vector>
-#include<iostream>
-#include<condition_variable>
-#include<thread>
+#include <vector>
 #include<mutex>
-#include<string>
-#include<algorithm>
+#include<thread>
+#include <algorithm>
+#include <iostream>
 
-class observe{
+class observer {
 public:
-    virtual void update(std::string name,double price);
+    virtual void update(double cost)=0;
+    virtual ~observer()=default;
 };
 
-class subject{
-    protected:
-        std::vector<observe*> observes;
-        std::mutex mutex_;
-        std::condition_variable cv;
-        bool price_change;
-    public:
-        subject():price_change(false){};
-
-        //添加观察者
-        void attach(observe* ob){
-            std::lock_guard<std::mutex> guard(mutex_);
-            observes.push_back(ob);
-        }
-        //删除
-        void detach(observe* ob){
-            std::lock_guard<std::mutex> guard(mutex_);
-            auto it=std::find(observes.begin(),observes.end(),ob);
-            if(it!=observes.end()){
-                observes.erase(it);
-            }
-        }
-
-        virtual void notify_all(std::string stockname,double price);
-        virtual void set_price(std::string stockname,double price);
+class subject {
+public:
+    std::vector<observer*> vector_;
+    virtual void add(observer*);
+    virtual void deleted(observer*);
+    virtual void notifyAll();
+    virtual ~subject()=default;
 };
 
-class concreate:public subject{
-    public:
-        void notify_all (std::string stockname,double price) override{
-            std::unique_lock<std::mutex> lock(mutex_);
-            cv.wait(lock,[this]{return price_change;});
-            for(observe* observer:observes){
-                observer->update(stockname,price);
-            }
-            price_change=false;
-        }
-        void set_price(std::string stockname,double price) override{
-            {
-                std::unique_lock<std::mutex> guard(mutex_);
-                price_change=true;
-            }
-            cv.notify_all();
-        }
-};
-
-//观察者，股票用户
-class ob1:public observe{
-    private:
-        std::string name;
-    public:
-        ob1(std::string name):name(name){};
-        void update(std::string stockname,double price) override{
-            std::cout<<name<<"received update:"<<stockname<<"price is now:"<<price<<std::endl;
-        }
-};
-
-//模拟价格变换的函数
-void update_price(subject* sub,std::string name){
-    double price=100.0;
-    while(true){
-        price+=(rand()%10-5)/10.0;
-        sub->set_price(name,price);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+class concreat:public subject {
+private:
+    int price;
+    std::mutex mutex_;
+public:
+    void add(observer* ob) override {
+        std::lock_guard<std::mutex> guard(mutex_);
+        vector_.push_back(ob);
     }
+    void deleted(observer* ob) override {
+        std::lock_guard<std::mutex> guard(mutex_);
+        auto it=std::find(vector_.begin(),vector_.end(),ob);
+        if(it!=vector_.end()) {
+            vector_.erase(it);
+        }
+    }
+    void notifyAll() override {
+        for(observer* ob:vector_) {
+            ob->update(price);
+        }
+    }
+    void set(int price) {
+        this->price=price;
+    }
+};
+class street:public observer {
+private:
+    double price=0;
+    subject& s;
+public:
+    street(subject& sub):s(sub) {
+            s.add(this);
+    }
+    void update(double cost) override {
+        std::cout<<"price is:"<<cost*2<<std::endl;
+    }
+     ~street() {
+        s.deleted(this);
+    }
+};
+
+class wall:public observer {
+private:
+    double price=0;
+    subject& s;
+public:
+    wall(subject& sub):s(sub) {
+        s.add(this);
+    }
+    void update(double cost) override {
+        std::cout<<"price is:"<<cost*2<<std::endl;
+    }
+    ~wall() {
+        s.deleted(this);
+    }
+};
+
+void addp(concreat& sub) {
+    street st(sub);
+    sub.set(200);
+    sub.notifyAll();
 }
 
-int main(int argc, char const *argv[])
-{
-    std::cout<<"xxxx"<<std::endl;
-    concreate c1;
-    ob1 a("mike");
-    ob1 b("jack");
-    ob1 c("lee");
+int main(int argc, char *argv[]) {
+    concreat sub;
+    std::thread t(addp,std::ref(sub));
 
-    c1.attach(&a);
-    c1.attach(&b);
-    c1.attach(&c);
+    wall w(sub);
+    sub.set(400);
+    sub.notifyAll();
 
-    std::string stockname="crop";
-
-    std::thread change(update_price,&c1,stockname);
-
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-
-    c1.detach(&b);
-    
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    change.join();
-    return 0;
+    t.join();
 }
-
